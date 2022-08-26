bb141c8f3d47d9d8475efb3cce2aea59a7d37a94
Peter Fitch <ptmfitch@users.noreply.github.com>
2022-08-26 16:32:16 +0100

    snake movement logic

--- game_of_life.py
+++ game_of_life.py
@@ -3,209 +3,231 @@ from time import time
 
 from pymongo import MongoClient, ASCENDING
 
-DB = "game_of_life"
-COLL = "coll"
-START_RATIO_ALIVE_CELLS = 0.4
-SIZE_X = 50
-SIZE_Y = 50
-NB_GEN = 50  # nb of generation we are going to calculate.
+# TODO make this config a queryable document
+DB = "snake"
+COLL = "grid"
+SIZE_X = 10
+SIZE_Y = 10
+SNAKE_START_SIZE = 3
+REFRESH_SECONDS = 10 # Fastest refresh in charts is every 10s
 
 
-def init_grid(coll):
-    coll.delete_many({})
-    coll.insert_one({})  # Create an empty doc in the collection for our init aggregation pipeline
-    pipeline = [
-        {
-            '$set': {
-                'x': {
-                    '$range': [
-                        0, SIZE_X
-                    ]
-                },
-                'y': {
-                    '$range': [
-                        0, SIZE_Y
-                    ]
-                }
+def init_grid(db):
+
+  print("Dropping existing collection")
+  db.drop_collection(COLL)
+
+  print("Creating a new collection")
+  collection = db.create_collection(COLL)
+
+  print("Inserting empty document into collection")
+  collection.create_index([("turn", -1)])
+  collection.insert_one({"turn": 0})
+
+  pipeline = [
+    { '$set': {
+      # Randomly place the first egg anywhere
+      # TODO improve by using a random selection
+      # from a valid array of tiles
+      'egg': {
+        'x': { '$floor': { '$multiply': [ { '$rand': {} }, SIZE_X ] } },
+        'y': { '$floor': { '$multiply': [ { '$rand': {} }, SIZE_Y ] } }
+      }
+    } }, 
+    { '$set': {
+      'snake': {
+        '$let': {
+          'vars': {
+            # The snake always starts pointing left (negative x),
+            # so needs to be at least SNAKE_START_SIZE away from
+            # the right hand edge
+            # TODO improve by using a random selection from a valid array
+            # of tile windows
+            'x': { '$floor': { '$multiply': [ 
+              { '$rand': {} }, 
+              { '$subtract': [ SIZE_X, SNAKE_START_SIZE ] }
+            ] } },
+            # To avoid the snake spawning on top of the egg, the
+            # initial y value should be on the opposite end
+            'y': {
+              '$cond': {
+                'if': { '$gt': [ '$egg.y', { '$divide': [ SIZE_Y, 2 ] } ] },
+                'then': { '$floor': { '$multiply': [ 
+                  { '$rand': {} },
+                  { '$divide': [ SIZE_Y, 2 ] } 
+                ] } },
+                'else': { '$add': [
+                  { '$floor': { '$multiply': [ 
+                    { '$rand': {} },
+                    { '$divide': [ SIZE_Y, 2 ] } 
+                  ] } },
+                  { '$divide': [ SIZE_Y, 2 ] }
+                ] }
+              }
             }
-        }, {
-            '$unwind': {
-                'path': '$x'
+          },
+          # The snake will start with its head to the left and
+          # tail extending to the right
+          'in': {
+            '$map': {
+              'input': { '$range': [0, SNAKE_START_SIZE] },
+              'in': {
+                'x': { '$add': [ '$$x', '$$this' ] },
+                'y': '$$y'
+              }
             }
-        }, {
-            '$unwind': {
-                'path': '$y'
-            }
-        }, {
-            '$set': {
-                'alive': {
-                    '$lt': [
-                        {
-                            '$rand': {}
-                        }, START_RATIO_ALIVE_CELLS
-                    ]
-                }
-            }
-        }, {
-            '$project': {
-                '_id': 0
-            }
-        }, {
-            '$out': COLL
+          }
         }
-    ]
-    start = time()
-    coll.aggregate(pipeline)
-    coll.create_index([('alive', ASCENDING), ('x', ASCENDING), ('y', ASCENDING)])
-    print('Grid init done in', round(time() - start, 2), 's')
+      }
+    } },
+    { '$set': {
+      # Construct a grid to display in charts (requires a double $unwind)
+      'grid': { '$map': {
+        'input': { '$range': [ 0, SIZE_X ] },
+        'as': 'x',
+        'in': { '$map': {
+          'input': { '$range': [ 0, SIZE_Y ] },
+          'as': 'y',
+          'in': {
+            'x': '$$x',
+            'y': '$$y',
+            'colour': { '$cond': {
+              'if': { '$in': [ { 'x': '$$x', 'y': '$$y' }, '$snake' ] },
+              'then': 0,
+              'else': { '$cond': {
+                'if': { '$eq': [ { 'x': '$$x', 'y': '$$y' }, '$egg' ] },
+                'then': 1,
+                'else': 2
+              } }
+            } }
+          }
+        } }
+      } }
+    } },
+    { '$merge': COLL }
+  ]
+  start = time()
+  collection.aggregate(pipeline)
+  print('Grid init done in', round(time() - start, 2), 's')
 
 
-def next_generation(coll):
-    pipeline = [
-        {
-            '$lookup': {
-                'from': 'coll',
-                'let': {
-                    'id': '$_id',
-                    'xx': '$x',
-                    'yy': '$y',
-                    'aa': '$alive'
-                },
-                'pipeline': [
-                    {
-                        '$match': {
-                            '$expr': {
-                                '$and': [
-                                    {
-                                        '$ne': [
-                                            '$_id', '$$id'
-                                        ]
-                                    }, {
-                                        '$eq': [
-                                            '$alive', True
-                                        ]
-                                    }, {
-                                        '$lte': [
-                                            '$x', {
-                                                '$add': [
-                                                    '$$xx', 1
-                                                ]
-                                            }
-                                        ]
-                                    }, {
-                                        '$lte': [
-                                            '$y', {
-                                                '$add': [
-                                                    '$$yy', 1
-                                                ]
-                                            }
-                                        ]
-                                    }, {
-                                        '$gte': [
-                                            '$x', {
-                                                '$add': [
-                                                    '$$xx', -1
-                                                ]
-                                            }
-                                        ]
-                                    }, {
-                                        '$gte': [
-                                            '$y', {
-                                                '$add': [
-                                                    '$$yy', -1
-                                                ]
-                                            }
-                                        ]
-                                    }
-                                ]
-                            }
-                        }
-                    }, {
-                        '$count': 'nb_neighbours_alive'
-                    }
-                ],
-                'as': 'neighbours'
-            }
-        }, {
-            '$replaceRoot': {
-                'newRoot': {
-                    '$mergeObjects': [
-                        {
-                            '$arrayElemAt': [
-                                '$neighbours', 0
-                            ]
-                        }, '$$ROOT'
-                    ]
-                }
-            }
-        }, {
-            '$addFields': {
-                'alive': {
-                    '$switch': {
-                        'branches': [
-                            {
-                                'case': {
-                                    '$and': [
-                                        {
-                                            '$eq': [
-                                                '$alive', True
-                                            ]
-                                        }, {
-                                            '$in': [
-                                                '$nb_neighbours_alive', [
-                                                    2, 3
-                                                ]
-                                            ]
-                                        }
-                                    ]
-                                },
-                                'then': True
-                            }, {
-                                'case': {
-                                    '$and': [
-                                        {
-                                            '$eq': [
-                                                '$alive', False
-                                            ]
-                                        }, {
-                                            '$eq': [
-                                                '$nb_neighbours_alive', 3
-                                            ]
-                                        }
-                                    ]
-                                },
-                                'then': True
-                            }
-                        ],
-                        'default': False
-                    }
-                }
-            }
-        }, {
-            '$project': {
-                'neighbours': 0,
-                'nb_neighbours_alive': 0,
-                'x': 0,
-                'y': 0
-            }
-        }, {
-            '$merge': {
-                'into': 'coll',
-                'on': '_id',
-                'whenMatched': 'merge',
-                'whenNotMatched': 'fail'
-            }
+def next_turn(db):
+  pipeline = [
+    # Grab the most recent turn document
+    { '$sort': { 'turn': -1 } },
+    { '$limit': 1 },
+    # Snake movement logic, effectively promote the last
+    # element to a new head, moving towards the egg
+    { '$set': {
+      # Increment the turn number while we're here
+      'turn': { '$add': [ '$turn', 1 ] },
+      # Set the new values for the snake array
+      'snake': {
+        '$let': {
+          'vars': {
+            # Borrowing some Seq names from Scala
+            # HTT (head, tail)
+            # IIL (init, last)
+            'head': { '$first': '$snake' },
+            'init': { '$slice': [ 
+              '$snake',
+              0, { '$subtract': [ { '$size': '$snake' }, 1 ] }
+            ] }
+          },
+          'in': { '$cond': {
+            # Attempt to move towards negative x (left)
+            'if': { '$and': [
+              # Check the egg is in this direction
+              { '$gt': [ '$$head.x', '$egg.x' ] },
+              # Check the tile to move to isn't part of the snake
+              # TODO remove one check from each turn by not checking
+              # the head (can't move to nowhere)
+              { '$not': { '$in': [
+                { 'x': { '$subtract': [ '$$head.x', 1 ] }, 'y': '$$head.y' }, 
+                '$$init' # Don't worry about the last segment
+              ] } }
+            ] },
+            'then': { '$concatArrays': [
+              [ { 'x': { '$subtract': [ '$$head.x', 1 ] }, 'y': '$$head.y' } ],
+              '$$init'
+            ] },
+            'else': { '$cond': {
+              # Attempt to move towards positive x (right)
+              'if': { '$and': [
+                { '$lt': [ '$$head.x', '$egg.x' ] },
+                { '$not': { '$in': [
+                  { 'x': { '$add': [ '$$head.x', 1 ] }, 'y': '$$head.y' }, 
+                  '$$init'
+                ] } }
+              ] },
+              'then': { '$concatArrays': [
+                [ { 'x': { '$add': [ '$$head.x', 1 ] }, 'y': '$$head.y' } ],
+                '$$init'
+              ] },
+              'else': { '$cond': {
+                # Attempt to move towards negative y (up)
+                'if': { '$and': [
+                  { '$gt': [ '$$head.y', '$egg.y' ] },
+                  { '$not': { '$in': [
+                    { 'x': '$$head.x', 'y': { '$subtract': [ '$$head.y', 1 ] } }, 
+                    '$$init'
+                  ] } }
+                ] },
+                'then': { '$concatArrays': [
+                  [ { 'x': '$$head.x', 'y': { '$subtract': [ '$$head.y', 1 ] } } ],
+                  '$$init'
+                ] },
+                # Move towards positive y (down)
+                # If we can't then we're dead...
+                'else': { '$concatArrays': [
+                  [ { 'x': '$$head.x', 'y': { '$add': [ '$$head.y', 1 ] } } ],
+                  '$$init'
+                ] }
+              } }
+            } }
+          } }
         }
-    ]
-    start = time()
-    coll.aggregate(pipeline)
-    print('New generation calculated in', round(time() - start, 2), 's')
+      }
+    } },
+    # Eat the egg logic
+    # Death logic
+    # Regenerate the grid
+    { '$set': {
+      # Construct a grid to display in charts (requires a double $unwind)
+      'grid': { '$map': {
+        'input': { '$range': [ 0, SIZE_X ] },
+        'as': 'x',
+        'in': { '$map': {
+          'input': { '$range': [ 0, SIZE_Y ] },
+          'as': 'y',
+          'in': {
+            'x': '$$x',
+            'y': '$$y',
+            'colour': { '$cond': {
+              'if': { '$in': [ { 'x': '$$x', 'y': '$$y' }, '$snake' ] },
+              'then': 0,
+              'else': { '$cond': {
+                'if': { '$eq': [ { 'x': '$$x', 'y': '$$y' }, '$egg' ] },
+                'then': 1,
+                'else': 2
+              } }
+            } }
+          }
+        } }
+      } }
+    } },
+    { '$merge': COLL }
+  ]
+  start = time()
+  db.get_collection(COLL).aggregate(pipeline)
+  print('Next turn calculated in', round(time() - start, 2), 's')
+
 
 
 def check_mongodb_uri():
-    if len(sys.argv) != 2:
-        print('MongoDB URI is missing in cmd line arg 1.')
-        exit(1)
+  if len(sys.argv) != 2:
+    print('MongoDB URI is missing in cmd line arg 1.')
+    exit(1)
 
 
 def get_mongodb_client(uri):
@@ -215,8 +237,11 @@ def get_mongodb_client(uri):
 if __name__ == '__main__':
     check_mongodb_uri()
     client = get_mongodb_client(sys.argv[1])
-    collection = client.get_database(DB).get_collection(COLL)
-    init_grid(collection)
-    for _ in range(NB_GEN):
-        input('Hit Enter for the next generation...')
-        next_generation(collection)
+    db = client.get_database(DB)
+    init_grid(db)
+    while(True):
+      input('Hit Enter for the next generation...')
+      next_turn(db)
+    # while(True): # TODO game win or loss condition
+    #   time.sleep(10)
+    #   next_turn(collection)
